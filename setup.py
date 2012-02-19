640e7e99ba094091a5d4705950c1a5a637b5b8c6
Dominic Scheirlinck <dominic@varspool.com>
2012-02-20 00:37:55 +1300

    Added classifiers

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 import sys
 from distutils.core import setup
+import musicdir
 
 if sys.version < '2.7':
     print "musicdir requires Python 2.7+"
@@ -9,14 +10,22 @@ if sys.version < '2.7':
 
 setup(
         name             = 'musicdir',
-        version          = '0.0.1',
+        version          = musicdir.version,
         description      = 'Symlink-mirrors directories of tagged music',
         long_description = open('README.md').read(),
         author           = 'Dominic Scheirlinck',
         author_email     = 'dominic@varspool.com',
         url              = 'http://github.com/dominics/musicdir',
+        license          = 'GNU General Public License (GPL) 3+',
         packages         = ['musicdir'],
         package_data     = {'musicdir': ['data/*.cfg']},
         scripts          = ['scripts/musicdir'],
-        requires         = ['mutagen (>=1.10.1)', 'argparse', 'ConfigParser']
+        requires         = ['mutagen (>=1.10.1)', 'argparse', 'ConfigParser'],
+        classifiers      = [
+            'Environment :: Console',
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: End Users/Desktop',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Topic :: Multimedia :: Sound/Audio :: Conversion',
+        ]
      )
